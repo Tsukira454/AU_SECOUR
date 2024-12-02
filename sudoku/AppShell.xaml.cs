52a63579144ba56ee548923280c983a003def27c
Elvin Gamecraft <elvingamecraft@hotmail.com>
2024-12-02 17:36:13 +0100

    Ajoutez des fichiers projet.

---
+++ sudoku/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace sudoku
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ sudoku/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace sudoku
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
